53747ad97e92107dad869554e268d660a38b72e0
Weston Bodily <westonbodily7z@gmail.com>
2018-09-17 16:26:02 -0600

    minimap visual improvements still in progress

--- Unity/SeedQuest/Assets/Shared/Scripts/Camera/MinimapCamera.cs
+++ Unity/SeedQuest/Assets/Shared/Scripts/Camera/MinimapCamera.cs
@@ -18,7 +18,11 @@ public class MinimapCamera : MonoBehaviour {
     public int resWidth = 2550;
     public int resHeight = 2550;
 
+    private int counter = 0;
+    private bool once = false;
+
     public Image screenShotRender;
+    public Texture screenTexture;
 
 
 	void Start () 
@@ -26,32 +30,67 @@ public class MinimapCamera : MonoBehaviour {
         cameraOffset.y = 100;
         renderComponent = GetComponent<RenderTexture>();
 
-        // ScreenShot();
+        //ScreenShot();
 	}
-	
+
+	private void Awake()
+	{
+        //ScreenShot();
+	}
+
 
 	void LateUpdate () 
     {
         transform.position = player.transform.position;
         transform.position += cameraOffset;
+
+        counter += 1;
+        //Debug.Log(counter);
+        if (counter >= 2 && once == false)
+        {
+            ScreenShot();
+            counter = 0;
+            once = true;
+        }
 	}
 
     // Functions attempting to make an image for the minimap camera to follow
     void ScreenShot()
     {
-        //transform.position = ;
+        transform.position = new Vector3(0,500,0);
         Camera cameraRef = GetComponent<Camera>();
         RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
         cameraRef.targetTexture = rt;
+        RenderTexture.active = rt;
         Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
         cameraRef.Render();
         //RenderTexture.active = rt;
         screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+        screenShot.Apply();
+
+        //GameObject miniShot = GameObject.Instantiate(screenShotRender);
+        screenShotRender.GetComponent<Image>().sprite = Sprite.Create(screenShot, new Rect(0, 0, resWidth, resHeight), new Vector2(0, 0));
+
+
         cameraRef.targetTexture = null;
         RenderTexture.active = null;
         Destroy(rt);
 
         cameraRef.targetTexture = renderComponent;
+        byte[] bytes = screenShot.EncodeToPNG();
+
+        string filename = "TestScreenShot.png";
+        System.IO.File.WriteAllBytes(filename, bytes);
+
+        // This doesn't quite get the job done
+
+        //panel.SetActive(true);
+        //panel.transform.position = transform.position;
+        //panel.transform.position -= new Vector3(0, 20, 0);
+
+        screenShotRender.GetComponent<Image>();
+                        
+        //panel.GetComponent<Material>().SetTexture("_MainTex", screenShot);
     }
 
 
---
+++ Unity/SeedQuest/Assets/Shared/Scripts/minimapImage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class minimapImage : MonoBehaviour {
+
+    public GameObject Player;
+
+    public int yScale;
+    public int xScale;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.position = new Vector3(-(Player.transform.position.x * xScale), -(Player.transform.position.z * yScale), 0);
+	}
+}
